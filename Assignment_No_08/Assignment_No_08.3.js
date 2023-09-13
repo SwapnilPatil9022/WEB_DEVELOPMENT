4b4aa9a6ca996327c75509b2f2d1f45b1a4fa33e
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-09-13 21:32:47 +0530

    Add files via upload

---
+++ Assignment_No_08/Assignment_No_08.3.js
@@ -0,0 +1,6 @@
+// var x = [10,20,True,"Web Development",3.15];
+//  from the given array find 4th element using array index.
+
+var X = [10, 20, "True","Web Development", 3.14];
+
+console.log(`Fourth Array Index = ${X[4]}`);
