1b8b0541ef6e3ba3a0ca54a7cc0dd74588d5f41d
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-09-13 21:31:49 +0530

    Delete Assignment_No_08.3.js

--- Assignment_No_08.3.js
+++
@@ -1,6 +0,0 @@
-// var x = [10,20,True,"Web Development",3.15];
-//  from the given array find 4th element using array index.
-
-var X = [10, 20, "True","Web Development", 3.14];
-
-console.log(`Fourth Array Index = ${X[4]}`);
