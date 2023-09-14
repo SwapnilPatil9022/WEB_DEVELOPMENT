6d4adf32b4edc3e91cf0a803aa573d6eb9fd6b17
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-09-14 22:31:54 +0530

    Add files via upload

---
+++ Assignment_No_08/Assignment_No_08.4.js
@@ -0,0 +1,10 @@
+// var X = [10.20, 10.30, 50.40, 70.50, 60.20]; 
+// Change the 4th array index with the float number 100.50 and print newly created array on browser window and console tab.
+
+var Y = [10.20, 10.30, 50.40, 70.50, 60.20];
+
+console.log(`Befoe Replace Index Value= ${Y}`);
+
+Y[4]=100.50;
+
+console.log(`After Replace 4th Index Value= ${Y}`);
