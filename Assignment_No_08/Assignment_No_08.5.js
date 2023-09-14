67469f027b5038903789ac876d504d7f5b249c3a
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-09-14 22:32:32 +0530

    Add files via upload

---
+++ Assignment_No_08/Assignment_No_08.5.js
@@ -0,0 +1,9 @@
+
+// Write a javascript program which prints last array element of the following array without using a any loop.
+// var a = [[1,20,30,40],[54,69,74,25],[56,74,23,69,89],[56,89,12,36,74]];
+
+var a = [[10, 20,30,40],[54,69,74,25],[56,74,23,69,89],[56,89,12,36,74]];
+
+let lastElement=a[a.length-1];    
+
+console.log(`Last Array Index = ${lastElement}`);
