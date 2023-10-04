1bc7d26e2400ae5ea774b9351ac59141aef22fb5
Swapnil Patil <105672771+SwapnilPatil9022@users.noreply.github.com>
2023-10-04 12:59:39 +0530

    Add files via upload

---
+++ Assignment_No_10/Assignment_No_10.2.js
@@ -0,0 +1,12 @@
+// Write Program to print “WELCOME” message 10 times using while and for loop.
+
+function Display()
+{
+    var i = 1;
+    while(i <= 10)
+    {
+        console.log("WELCOME");
+        i++;
+    }
+}
+Display();
